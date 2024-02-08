07ae0167d098d230788d5af6552ff36f7d81a693
elenadani <149767181+elenadani@users.noreply.github.com>
2024-02-08 22:36:03 +0300

    Add files via upload

--- app_runner.py
+++ app_runner.py
@@ -8,77 +8,35 @@ app = Flask(__name__)
 def render_map():
     con = sqlite3.connect("map_db.sqlite")
     cur = con.cursor()
-    result_1 = cur.execute("""SELECT thing FROM Invention""").fetchall()
-    result_2 = cur.execute("""SELECT year FROM Invention""").fetchall()
-    result_3 = cur.execute("""SELECT coord_1 FROM Invention""").fetchall()
-    result_4 = cur.execute("""SELECT coord_2 FROM Invention""").fetchall()
-    result_5 = cur.execute("""SELECT URL_picture FROM Invention""").fetchall()
-    result_6 = cur.execute("""SELECT icon FROM Invention""").fetchall()
-
-    map_dict = dict()
-    map_dict['name'] = [result_1[i][0] for i in range(len(result_1))]
-    map_dict['year'] = [result_2[i][0] for i in range(len(result_2))]
-    map_dict['coord_1'] = [result_3[i][0] for i in range(len(result_3))]
-    map_dict['coord_2'] = [result_4[i][0] for i in range(len(result_4))]
-    map_dict['URL_picture'] = [result_5[i][0] for i in range(len(result_5))]
-    map_dict['icon'] = [result_6[i][0] for i in range(len(result_6))]
-
+    result = cur.execute("""SELECT * FROM Invention WHERE id BETWEEN 1 AND 15""").fetchall()
     con.commit()
     con.close()
-    return render_template('map.html', map_dict=map_dict)
+    return render_template('map.html', map_dict=result)
 
 
 @app.route('/19_century')
 def render_map_1():
     con = sqlite3.connect("map_db.sqlite")
     cur = con.cursor()
-    result_1 = cur.execute("""SELECT thing FROM Invention WHERE century <= 1900""").fetchall()
-    result_2 = cur.execute("""SELECT year FROM Invention WHERE century <= 1900""").fetchall()
-    result_3 = cur.execute("""SELECT coord_1 FROM Invention WHERE century <= 1900""").fetchall()
-    result_4 = cur.execute("""SELECT coord_2 FROM Invention WHERE century <= 1900""").fetchall()
-    result_5 = cur.execute("""SELECT URL_picture FROM Invention WHERE century <= 1900""").fetchall()
-    result_6 = cur.execute("""SELECT icon FROM Invention WHERE century <= 1900""").fetchall()
-
-    map_dict = dict()
-    map_dict['name'] = [result_1[i][0] for i in range(len(result_1))]
-    map_dict['year'] = [result_2[i][0] for i in range(len(result_2))]
-    map_dict['coord_1'] = [result_3[i][0] for i in range(len(result_3))]
-    map_dict['coord_2'] = [result_4[i][0] for i in range(len(result_4))]
-    map_dict['URL_picture'] = [result_5[i][0] for i in range(len(result_5))]
-    map_dict['icon'] = [result_6[i][0] for i in range(len(result_6))]
-
+    result = cur.execute("""SELECT * FROM Invention WHERE (century < 1901) AND id BETWEEN 1 AND 15""").fetchall()
     con.commit()
     con.close()
-    return render_template('map.html', map_dict=map_dict)
+    return render_template('map.html', map_dict=result)
 
 
 @app.route('/20_century')
 def render_map_2():
     con = sqlite3.connect("map_db.sqlite")
     cur = con.cursor()
-    result_1 = cur.execute("""SELECT thing FROM Invention WHERE century > 1900""").fetchall()
-    result_2 = cur.execute("""SELECT year FROM Invention WHERE century > 1900""").fetchall()
-    result_3 = cur.execute("""SELECT coord_1 FROM Invention WHERE century > 1900""").fetchall()
-    result_4 = cur.execute("""SELECT coord_2 FROM Invention WHERE century > 1900""").fetchall()
-    result_5 = cur.execute("""SELECT URL_picture FROM Invention WHERE century > 1900""").fetchall()
-    result_6 = cur.execute("""SELECT icon FROM Invention WHERE century > 1900""").fetchall()
-
-    map_dict = dict()
-    map_dict['name'] = [result_1[i][0] for i in range(len(result_1))]
-    map_dict['year'] = [result_2[i][0] for i in range(len(result_2))]
-    map_dict['coord_1'] = [result_3[i][0] for i in range(len(result_3))]
-    map_dict['coord_2'] = [result_4[i][0] for i in range(len(result_4))]
-    map_dict['URL_picture'] = [result_5[i][0] for i in range(len(result_5))]
-    map_dict['icon'] = [result_6[i][0] for i in range(len(result_6))]
-
+    result = cur.execute("""SELECT * FROM Invention WHERE (century > 1900) AND id BETWEEN 1 AND 15""").fetchall()
     con.commit()
     con.close()
-    return render_template('map.html', map_dict=map_dict)
+    return render_template('map.html', map_dict=result)
 
 
 @app.route('/about')
 def about_project():
-    return "Hello"
+    return render_template('about_project.html')
 
 
 if __name__ == "__main__":
