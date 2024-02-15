e78ef72b7dbb45be64264a6b91ed70c8fe17111c
elenadani <149767181+elenadani@users.noreply.github.com>
2024-02-15 23:48:01 +0300

    Delete geo.py

--- geo.py
+++
@@ -1,6 +0,0 @@
-from geopy.geocoders import Nominatim
-
-geolocator = Nominatim(user_agent="coordinates_finder")
-location = geolocator.geocode("Moscow")
-print(location.address)
-print((location.latitude, location.longitude))
